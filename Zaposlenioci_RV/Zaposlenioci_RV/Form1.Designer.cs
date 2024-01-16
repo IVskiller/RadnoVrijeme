03224491e888391463384523fd404d4e3412daf8
IVskiller <112958685+IVskiller@users.noreply.github.com>
2024-01-16 19:37:38 +0100

    Add files via upload

---
+++ Zaposlenioci_RV/Zaposlenioci_RV/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿
+namespace Zaposlenioci_RV {
+    partial class Form1 {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxfirme = new System.Windows.Forms.ComboBox();
+            this.comboBoxZaposlenici = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.debugToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBoxfirme
+            // 
+            this.comboBoxfirme.FormattingEnabled = true;
+            this.comboBoxfirme.Location = new System.Drawing.Point(24, 44);
+            this.comboBoxfirme.Name = "comboBoxfirme";
+            this.comboBoxfirme.Size = new System.Drawing.Size(230, 23);
+            this.comboBoxfirme.TabIndex = 0;
+            this.comboBoxfirme.SelectedIndexChanged += new System.EventHandler(this.comboBoxfirme_SelectedIndexChanged);
+            // 
+            // comboBoxZaposlenici
+            // 
+            this.comboBoxZaposlenici.FormattingEnabled = true;
+            this.comboBoxZaposlenici.Location = new System.Drawing.Point(24, 86);
+            this.comboBoxZaposlenici.Name = "comboBoxZaposlenici";
+            this.comboBoxZaposlenici.Size = new System.Drawing.Size(121, 23);
+            this.comboBoxZaposlenici.TabIndex = 1;
+            this.comboBoxZaposlenici.SelectedIndexChanged += new System.EventHandler(this.comboBoxZaposlenici_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Dolazak";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(138, 137);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Odlazak";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(696, 415);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = ">";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker1.Location = new System.Drawing.Point(606, 415);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.ShowUpDown = true;
+            this.dateTimePicker1.Size = new System.Drawing.Size(67, 23);
+            this.dateTimePicker1.TabIndex = 6;
+            this.dateTimePicker1.Value = new System.DateTime(2024, 1, 11, 19, 35, 0, 0);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Checked = false;
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker2.Location = new System.Drawing.Point(245, 413);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(148, 23);
+            this.dateTimePicker2.TabIndex = 7;
+            this.dateTimePicker2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(459, 421);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(459, 390);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 15);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "label3";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(489, 39);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(299, 304);
+            this.listBox1.TabIndex = 12;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 13;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.debugToolStripMenuItem});
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // debugToolStripMenuItem
+            // 
+            this.debugToolStripMenuItem.Checked = true;
+            this.debugToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.debugToolStripMenuItem.Name = "debugToolStripMenuItem";
+            this.debugToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.debugToolStripMenuItem.Text = "Debug";
+            this.debugToolStripMenuItem.Click += new System.EventHandler(this.debugToolStripMenuItem_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(314, 39);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(169, 304);
+            this.listBox2.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBoxZaposlenici);
+            this.Controls.Add(this.comboBoxfirme);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxfirme;
+        private System.Windows.Forms.ComboBox comboBoxZaposlenici;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem debugToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ Zaposlenioci_RV/Zaposlenioci_RV/Form1.cs
@@ -0,0 +1,409 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace Zaposlenioci_RV {
+    public partial class Form1 : Form {
+        DB nasabaza;
+        public Form1()
+        {
+            InitializeComponent();
+            nasabaza = new DB();
+            label1.Text = dateTimePicker2.Value.ToString("yyyy-MM-dd");
+            Refreshliste();
+          
+        }
+
+        public void Refreshliste() {
+            comboBoxfirme.Items.Clear();
+
+            List<string> firme = nasabaza.Select("select [ID_firme],[Naziv_firme] from Firme");
+            foreach (string firma in firme)
+            {
+                comboBoxfirme.Items.Add(firma);
+            }
+
+
+
+        }
+
+        public class Zaposlenik {
+            int ID_zaposlenika;
+            string Ime;
+            string Prezimeime;
+            int ID_firme;
+            List <DateTime> zakašnjenja = new List<DateTime>();
+            List<DateTime> raniodlasci = new List<DateTime>();
+
+            public Zaposlenik(int ID_zaposlenika,string Ime, string Prezimeime) {
+              this.ID_zaposlenika= ID_zaposlenika;
+                this.Ime= Ime;
+                this.Prezimeime= Prezimeime;
+                
+            }
+
+            public Zaposlenik(int ID_zaposlenika, string Ime, string Prezimeime, int ID_firme){
+                this.ID_zaposlenika = ID_zaposlenika;
+                this.Ime = Ime;
+                this.Prezimeime = Prezimeime;
+                this.ID_firme = ID_firme;
+
+            }
+
+        }
+
+      
+
+
+        public class DB {
+
+            private static DB instance;
+            private SqlConnection connection;
+            public List<Zaposlenik> Zaposlenici = new List<Zaposlenik>(); 
+
+            public DB()
+            {
+                string conn_string = @"Server=localhost\SQLEXPRESS;Database=pracenjeradjnogvremena;Trusted_Connection=True;User Id=Ivort;Password=sqlknjiznica";
+                connection = new SqlConnection(conn_string);
+                connection.Open();
+            }
+
+
+            public static DB Instance
+            {
+                get
+                {
+                    if (instance == null)
+                    {
+                        instance = new DB();
+                    }
+                    return instance;
+                }
+            }
+
+            public List<string> Select(string sql)
+            {
+                List<string> output = new List<string>();
+                SqlDataReader reader;
+                SqlCommand komanda = new SqlCommand(sql, connection);
+                reader = komanda.ExecuteReader();
+                string row;
+                while (reader.Read())
+                {
+                    int count = reader.FieldCount;
+                    row = "";
+                    for (int i = 0; i < count; i++)
+                    {
+                        row += reader.GetValue(i).ToString() + " ";
+                    }
+                    output.Add(row);
+
+                }
+                reader.Close();
+
+                return output;
+            }
+
+            public void Insert(string sql)
+            {
+               
+                SqlDataReader reader;
+                SqlCommand komanda = new SqlCommand(sql, connection);
+                reader = komanda.ExecuteReader();
+                reader.Close();
+            }
+
+
+
+            public void Update(string sql)
+            {
+
+                SqlDataReader reader;
+                SqlCommand komanda = new SqlCommand(sql, connection);
+                komanda.ExecuteNonQuery();
+
+            }
+
+
+            public void Delete(string sql)
+            {
+               
+                SqlDataReader reader;
+                SqlCommand komanda = new SqlCommand(sql, connection);
+                reader = komanda.ExecuteReader();
+                reader.Close();
+            }
+
+            public void zaposlenicirf()
+            {
+                Zaposlenici.Clear();
+                List<string> zaptemp = Select("select [ID_prijave] from Prijave");
+                foreach (string zaposlenik in zaptemp) {
+                    int id =   Int32.Parse(zaposlenik.Substring(0, zaposlenik.IndexOf(" ")));
+                    string ime= Select("select ime from Zaposlenici WHERE ID_Zaposlenika=" + id).ElementAt(0);
+                    string prezime = Select("select prezime from Zaposlenici WHERE ID_Zaposlenika=" + id).ElementAt(0);
+                    int firma = Int32.Parse(Select("select prezime from Zaposlenici WHERE ID_Zaposlenika=" + id).ElementAt(0));
+                    Zaposlenik z = new Zaposlenik(id,ime,prezime,firma);
+                    Zaposlenici.Add(z);
+                }
+
+            }
+
+
+
+        }
+
+
+        public string getdatum() { if (debugToolStripMenuItem.Checked) return label1.Text;
+            else
+            {
+                DateTime currentDateTime = DateTime.Now;
+                string a =currentDateTime.ToString("yyyy-MM-dd");
+                return a;
+            }
+        }
+        public string getvrijeme() {
+            if (debugToolStripMenuItem.Checked) return dateTimePicker1.Value.TimeOfDay.ToString();
+            else { DateTime currentDateTime = DateTime.Now;
+                return currentDateTime.ToString("HH:mm:ss");     }
+        }
+
+        public string getzaposlenik() { return comboBoxZaposlenici.Text.Substring(0, comboBoxfirme.Text.IndexOf(" ")).ToString(); }
+        public string getfirma() { return comboBoxfirme.Text.Substring(0, comboBoxfirme.Text.IndexOf(" ")).ToString(); }
+
+
+
+        public bool subotach() {
+            if (debugToolStripMenuItem.Checked)
+            {
+                if (dateTimePicker2.Value.DayOfWeek.ToString() == "Saturday") return true;
+                else return false;
+            }
+            else
+            {
+                DateTime currentDateTime = DateTime.Now;
+                if (currentDateTime.DayOfWeek.ToString() == "Saturday") return true;
+                else return false;
+            }
+
+        }
+
+
+
+        public bool nedeljach()
+        {
+            if (debugToolStripMenuItem.Checked)
+            {
+                if (dateTimePicker2.Value.DayOfWeek.ToString() == "Sunday") return true;
+                else return false;
+            }
+            else
+            {
+                DateTime currentDateTime = DateTime.Now;
+                if (currentDateTime.DayOfWeek.ToString() == "Sunday") return true;
+                else return false;
+            }
+        }
+
+        public void zaposlenici_kasne()
+        {
+            string mindatum = dateTimePicker2.Value.AddDays(-5).ToString("yyyy-MM-dd");
+            listBox1.Items.Clear();
+
+            List<string> zap_kasne = nasabaza.Select("SELECT Prijave.ID_Zaposlenika, Prijave.Vrsta_prijave,  Prijave.Vrijeme_prijave , Prijave.Datum_prijave FROM Firme, Zaposlenici, Prijave WHERE Zaposlenici.ID_firme = Firme.ID_firme AND Prijave.ID_Zaposlenika = Zaposlenici.ID_zaposlenika AND Prijave.Vrijeme_prijave > Firme.rv_poc_kasno AND Prijave.Vrsta_prijave = 'Prijava' AND Prijave.Datum_prijave>'" + mindatum + "' AND Prijave.Datum_prijave<'" + label1.Text + "'");
+            foreach (string zaposlnik in zap_kasne)
+            {
+                listBox1.Items.Add(zaposlnik.Substring(0, zaposlnik.Length - 8));
+            }
+
+        }
+
+        public void zaposlenici_raniodlazak()
+        {
+            string mindatum = dateTimePicker2.Value.AddDays(-5).ToString("yyyy-MM-dd");
+            List<string> zap_kasne = nasabaza.Select("SELECT Prijave.ID_Zaposlenika, Prijave.Vrsta_prijave,  Prijave.Vrijeme_prijave , Prijave.Datum_prijave FROM Firme, Zaposlenici, Prijave WHERE Zaposlenici.ID_firme = Firme.ID_firme AND Prijave.ID_Zaposlenika = Zaposlenici.ID_zaposlenika AND Prijave.Vrijeme_prijave < Firme.rv_zav_rano AND Prijave.Vrsta_prijave = 'Odjava' AND Prijave.Datum_prijave>'" + mindatum + "' AND Prijave.Datum_prijave<'" + label1.Text + "'");
+            foreach (string zaposlnik in zap_kasne)
+            {
+                listBox1.Items.Add(zaposlnik.Substring(0, zaposlnik.Length - 8));
+            }
+
+        }
+
+
+        public string getrvfirme() {
+
+            List<string> firmarv= nasabaza.Select("SELECT rv_zav_rano FROM Firme WHERE ID_firme="+ getfirma());
+            return firmarv.ElementAt(0);
+
+        }
+
+
+
+        public int otvorenich()
+        {
+            List<string> vrijemeradi = nasabaza.Select("SELECT Firme.rv_poc_rano FROM Firme WHERE Firme.ID_firme = " + getfirma());
+            TimeSpan time1 = new TimeSpan(Int32.Parse(vrijemeradi.ElementAt(0).Substring(0, 2)), (Int32.Parse(vrijemeradi.ElementAt(0).Substring(3, 2))), (Int32.Parse(vrijemeradi.ElementAt(0).Substring(6, 2))));
+            string trvrijeme = dateTimePicker1.Value.TimeOfDay.ToString();
+            TimeSpan time2 = new TimeSpan(Int32.Parse(trvrijeme.Substring(0, 2)), Int32.Parse(trvrijeme.Substring(3, 2)), Int32.Parse(trvrijeme.Substring(6, 2)));
+
+            int usporedba = TimeSpan.Compare(time1, time2);
+
+            return usporedba;
+        }
+
+        
+
+
+
+
+
+
+        private void comboBoxfirme_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBoxZaposlenici.ResetText();
+            List<string> zaposlenici = nasabaza.Select("select [ID_zaposlenika],[Ime],[Prezime] from Zaposlenici WHERE  ID_firme=" + comboBoxfirme.Text.Substring(0, comboBoxfirme.Text.IndexOf(" ")).ToString() + "");
+            comboBoxZaposlenici.Items.Clear();
+            foreach (string zaposlnik in zaposlenici)
+            {
+                comboBoxZaposlenici.Items.Add(zaposlnik);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dateTimePicker2.Value = dateTimePicker2.Value.AddDays(1);
+            label1.Text = dateTimePicker2.Value.ToString("yyyy-MM-dd");
+
+
+            if (subotach())
+            {
+                label2.Text = "Subota";
+                zaposlenici_kasne();
+                zaposlenici_raniodlazak();
+                comboBoxfirme.Visible = false;
+                comboBoxZaposlenici.Visible = false;
+                button1.Visible = false;
+                button2.Visible = false;
+                label3.Visible = false;
+
+            }
+            else if (nedeljach())
+            {
+                label2.Text = "Nedelja";
+
+            }
+            else
+            {
+                label2.Text = "Radni dan";
+                comboBoxfirme.Visible = true;
+                comboBoxZaposlenici.Visible = true;
+                button1.Visible = true;
+                button2.Visible = true;
+                label3.Visible = true;
+
+
+                List<string> prijavljenich = nasabaza.Select("select [ID_prijave] from Prijave WHERE  ID_zaposlenika =" + getzaposlenik() + " AND Datum_prijave='" + getdatum() + "'   AND Vrsta_prijave='Odjava'");
+                if (prijavljenich.Count == 0)
+                {
+                    nasabaza.Insert("INSERT INTO [dbo].[Prijave] ([ID_Zaposlenika],[Datum_prijave],[Vrijeme_prijave],[Vrsta_prijave]) VALUES (" + getzaposlenik() + ",'" + getdatum() + "','" + getrvfirme() + "' ,'Odjava');");
+                }
+
+            }
+
+            nasabaza.zaposlenicirf();
+            comboBoxfirme.ResetText();
+            comboBoxZaposlenici.ResetText();
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+
+        {
+
+
+            if (otvorenich() > 0)
+            {
+                label3.Text = "Još ne radimo";
+            }
+            else
+            {
+                nasabaza.Insert("INSERT INTO [dbo].[Prijave] ([ID_Zaposlenika],[Datum_prijave],[Vrijeme_prijave],[Vrsta_prijave]) VALUES (" + getzaposlenik() + ",'" + getdatum() + "','" + getvrijeme() + "' ,'Prijava');");
+
+
+                button2.Visible = true;
+                button1.Visible = false;
+                label3.Text = "";
+            }
+
+
+
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                nasabaza.Insert("INSERT INTO [dbo].[Prijave] ([ID_Zaposlenika],[Datum_prijave],[Vrijeme_prijave],[Vrsta_prijave]) VALUES (" + getzaposlenik() + ",'" + getdatum() + "','" + getvrijeme() + "' ,'Odjava');");
+                button2.Visible = false;
+            }
+            catch { }
+        }
+
+        private void comboBoxZaposlenici_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            List<string> prijavljenich = nasabaza.Select("select [ID_prijave] from Prijave WHERE  ID_zaposlenika =" + getzaposlenik() + " AND Datum_prijave='" + getdatum() + "'");
+            if (prijavljenich.Count == 0)
+            {
+                button1.Visible = true;
+            }
+
+
+
+        }
+
+        private void debugToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            if (debugToolStripMenuItem.Checked)
+            {
+                button3.Visible = false;
+                label1.Visible = false;
+                dateTimePicker1.Visible = false;
+                debugToolStripMenuItem.Checked = false;
+            }
+            else
+            {
+
+                button3.Visible = true;
+                label1.Visible = true;
+                dateTimePicker1.Visible = true;
+
+
+                debugToolStripMenuItem.Checked = true;
+            }
+
+        }
+
+       
+    }
+
+}
+    
